a15a7076e11cdf5c980748f8e17ea62510b8da25
balecore <balecored@gmail.com>
2026-09-08 13:29:00 +0500

    Разработанная страница Main. Шаповалов.

---
+++ ReportGeneration_Шаповалов/Pages/Main.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace ReportGeneration_Шаповалов.Pages
+{
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
